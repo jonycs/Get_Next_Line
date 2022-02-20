77ad88020fa79d27ffb3662124ed671f16eb5e59
Jonycs <jonatascstambouli@gmail.com>
2022-02-21 00:03:13 +0100

    Teste de código

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocorona <jocorona@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 22:43:08 by jocorona          #+#    #+#             */
+/*   Updated: 2022/02/20 23:59:47 by jocorona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t			ft_strlen(char *str);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+char			*get_next_line(int fd);
+char			*ft_get_line(char *save);
+char			*ft_save(char *save);
+char			*ft_read_and_save(int fd, char *save);
+
+#endif
